939e544af9b533cd8ab522d3e5d754050355e229
itsushashi-eng <itsushashi@gmail.com>
2025-08-05 22:42:11 +0600

    Removed head and others

--- main.c
+++ main.c
@@ -3,17 +3,9 @@
 
 int main()
 {
-<<<<<<< HEAD
-    printf("Hello world!\n");
-    printf("Testing out\n");
-    printf("Learning new thing\n");
-    printf("zarin mushraba shaira");
-
-=======
     printf("Krishi Darshan\n");
     printf("One stop solution for agriculture management\n\n");
 
     loginMenu();
->>>>>>> 775dd139e8d2564caf38c5e26c99fbdc2af21660
     return 0;
 }
