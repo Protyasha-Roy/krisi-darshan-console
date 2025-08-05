02537ece1d9a72aa5efec6276c08239427327b76
itsushashi-eng <itsushashi@gmail.com>
2025-08-05 21:42:22 +0600

    Changed nothing

--- main.c
+++ main.c
@@ -5,11 +5,8 @@ int main()
 {
     printf("Hello world!\n");
     printf("Testing out\n");
-<<<<<<< HEAD
-=======
     printf("Learning new thing\n");
->>>>>>> ef0943f8102418e1d187ed49695ac78707ebdaf9
+    printf("zarin mushraba shaira");
 
-printf("zarin mushraba shaira");
     return 0;
 }
