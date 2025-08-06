10ab2a32c9286430ab5537c7d014aefcc82ea618
Protyasha-Roy <protyasharoy369@gmail.com>
2025-08-07 00:49:25 +0600

    deletion of one file

---
+++ farmer.h
@@ -0,0 +1,10 @@
+#ifndef FARMER_H_INCLUDED
+#define FARMER_H_INCLUDED
+
+void personal_details(int id);
+void land_parcels(int id);
+void application_schemes(int id);
+void chatbot();
+void track_cropcycle(int id);
+
+#endif // FARMER_H_INCLUDED
